d7381f98106400818fc693eb9879c86e931a7ccb
Shinkai Setsuna <deanshin365@gmail.com>
2019-05-09 09:26:43 -0700

    correct styling

--- webapp/main.py
+++ webapp/main.py
@@ -1,20 +1,16 @@
 from flask import *
 from flask_socketio import SocketIO
 
-
 # Create flask app, SocketIO object, and global pi 'thing' object.
 app = Flask(__name__)
 socketio = SocketIO(app)
-
 
 # Define app routes.
 # Index route renders the main HTML page.
 @app.route("/")
 def index():
-    switch = 1
     # Render index.html template.
-    return render_template('index.html', switch=switch)
-
+    return render_template('index.html')
 
 if __name__ == "__main__":
     # Run the flask development web server with SocketIO.
